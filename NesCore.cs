f4b0671bfc003a37bd1dba340207a426eda28162
Filip Ekström <Filip.ekstrom98@gmail.com>
2022-05-20 20:14:34 +0200

    NesCore.cs was missing

---
+++ NesCore.cs
@@ -0,0 +1,89 @@
+﻿using BizHawk.Emulation.Common;
+using NesEmu.CPU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.SDL;
+
+namespace NesEmu {
+    internal class NesCore {
+        public Bus.Bus Bus;
+        public NesCpu CPU;
+        public PPU.PPU PPU;
+        public BizHawk.NES.APU APU;
+
+        const uint AUDIO_SAMPLE_FULL_THRESHOLD = 2048;
+        const int SAMPLES_PER_CALLBACK = 32;  // 735 = 44100 samples / 60 fps // 367.5? 1470
+
+        IntPtr audioBuffer = Marshal.AllocHGlobal(16384);
+        SDL_AudioSpec sdlWant, sdlHave;
+        int _audioDevice;
+
+        public NesCore(Rom.Rom rom) {
+            PPU = new PPU.PPU(rom.ChrRom, rom.Mirroring);
+            CPU = new NesCpu();
+            APU = new BizHawk.NES.APU(CPU, null, false);
+            Bus = new Bus.Bus(CPU, PPU, APU, rom);
+
+            CPU.RegisterBus(Bus);
+
+            int count = SDL_GetNumAudioDevices(0);
+            for (int i = 0; i < count; ++i) {
+                Console.WriteLine($"Device {i} {SDL_GetAudioDeviceName(i, 0)}");
+            }
+
+            sdlWant.channels = 2;
+            sdlWant.freq = 44100;
+            sdlWant.samples = (ushort)SAMPLES_PER_CALLBACK;
+            sdlWant.format = AUDIO_S16LSB;
+            var defaultDevice = SDL_GetAudioDeviceName(2, 0);
+            _audioDevice = (int)SDL_OpenAudioDevice(defaultDevice, 0, ref sdlWant, out sdlHave, 0); //(int)SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+
+            if (_audioDevice == 0) {
+                Console.WriteLine($"There was an issue opening the audio device. {SDL_GetError()}");
+            } else {
+                Console.WriteLine($"Audio Device Initialized: {SDL_GetAudioDeviceName((int)_audioDevice, 0)}");
+            }
+
+            SDL_PauseAudioDevice((uint)_audioDevice, 0);
+        }
+
+        /// <summary>
+        /// PPU has the fastest clock speed, so clock the ppu every clock here and then the others every so often
+        /// </summary>
+        public bool Clock() {
+            return Bus.Clock();
+        }
+
+        public void Reset() {
+            APU.NESHardReset();
+            CPU.Reset();
+        }
+
+        public void PlayFrameSamples() {
+            var count = APU.sampleclock;
+            Bus.blip.EndFrame(count);
+            APU.sampleclock = 0;
+            var numAvailable = Bus.blip.SamplesAvailable();
+            var samples = new short[numAvailable * 2];
+            Bus.blip.ReadSamples(samples, numAvailable, true);
+
+            for (int i = numAvailable - 1; i >= 0; i--) {
+                samples[i * 2] = samples[i];
+                samples[i * 2 + 1] = samples[i];
+            }
+
+            if ((SDL_GetQueuedAudioSize((uint)_audioDevice) / sizeof(short)) < AUDIO_SAMPLE_FULL_THRESHOLD) {
+                int bytes = sizeof(short) * samples.Length;
+                Marshal.Copy(samples, 0, audioBuffer, samples.Length);
+                //SDL_SetWindowTitle(window, $"{count} samples {numAvailable} available");
+                SDL_QueueAudio((uint)_audioDevice, audioBuffer, (uint)bytes);
+            } else {
+                var x = 5;
+            }
+        }
+    }
+}
