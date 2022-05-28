e56ea47c29146561718f70d69b466eb8e5661af3
Filip Ekström <Filip.ekstrom98@gmail.com>
2022-05-28 15:32:46 +0200

    Much more consistent audio with NAudio over SDL2

--- NesCore.cs
+++ NesCore.cs
@@ -1,4 +1,5 @@
 ﻿using BizHawk.Emulation.Common;
+using NAudio.Wave;
 using NesEmu.CPU;
 using System;
 using System.Collections.Generic;
@@ -9,14 +10,39 @@ using System.Threading.Tasks;
 using static SDL2.SDL;
 
 namespace NesEmu {
+    internal class BufferProvider : WaveProvider16 {
+        public Queue<short> internalBuffer = new Queue<short>();
+
+        public void Clear() {
+            internalBuffer.Clear();
+        }
+
+        public void TryQueue(short[] samples) {
+            for (var i = 0; i < samples.Length; i++) {
+                internalBuffer.Enqueue(samples[i]);
+            }
+        }
+        
+        public override int Read(short[] buffer, int offset, int sampleCount) {
+            Queue<short> outBuff = new Queue<short>();
+            var samplesTaken = Math.Min(sampleCount, internalBuffer.Count);
+            for (var i = 0; i < samplesTaken; i++) {
+                buffer[i] = internalBuffer.Dequeue();
+            }
+            return samplesTaken;
+        }
+    }
+
     internal class NesCore : IDisposable {
         public Bus.Bus Bus;
         public NesCpu CPU;
         public PPU.PPU PPU;
         public BizHawk.NES.APU APU;
 
-        SDL_AudioSpec sdlSpec;
-        int audioDevice;
+        int SamplesPerFrame = (44100 / 60) + 1;
+
+        DirectSoundOut dso;
+        BufferProvider bp;
 
         public NesCore(Rom.Rom rom) {
             PPU = new(rom.ChrRom);
@@ -27,65 +53,9 @@ namespace NesEmu {
             CPU.RegisterBus(Bus);
             PPU.RegisterBus(Bus);
 
-            Console.CursorTop++;
-
-            List<string> devices = new List<string>();
-            int count = SDL_GetNumAudioDevices(0);
-            for (int i = 0; i < count; ++i) {
-                devices.Add(SDL_GetAudioDeviceName(i, 0));
-            }
-
-            Console.WriteLine($"Select your audio device: ");
-
-            int selected = -1;
-            int marked = 0;
-            int initialRow = Console.CursorTop;
-            while (selected < 0) {
-                Console.CursorTop = initialRow;
-                var index = 0;
-                foreach (var dev in devices) {
-                    if (index == marked) {
-                        Console.BackgroundColor = ConsoleColor.DarkGreen;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.Write($"> {dev}\n");
-                        Console.BackgroundColor = ConsoleColor.Black;
-                        Console.ForegroundColor = ConsoleColor.White;
-                    } else {
-                        Console.Write($"  {dev}\n");
-                    }
-                    index++;
-                }
-
-                var nextKey = Console.ReadKey();
-                if (nextKey.Key == ConsoleKey.DownArrow) {
-                    if (marked == devices.Count - 1) {
-                        continue;
-                    }
-                    marked++;
-                } else if (nextKey.Key == ConsoleKey.UpArrow) {
-                    if (marked == 0) {
-                        continue;
-                    }
-                    marked--;
-                } else if (nextKey.Key == ConsoleKey.Enter) {
-                    selected = marked;
-                }
-            }
-            var usedDevice = devices[selected];
-
-            sdlSpec.channels = 2;
-            sdlSpec.freq = 44100;
-            sdlSpec.samples = 32;
-            sdlSpec.format = AUDIO_S16LSB;
-            audioDevice = (int)SDL_OpenAudioDevice(usedDevice, 0, ref sdlSpec, out SDL_AudioSpec received, 0);
-
-            if (audioDevice == 0) {
-                Console.WriteLine($"There was an issue opening the audio device. {SDL_GetError()}");
-            } else {
-                Console.WriteLine($"Audio Device Initialized: {usedDevice}");
-            }
-
-            SDL_PauseAudioDevice((uint)audioDevice, 0);
+            dso = new DirectSoundOut(60);
+            bp = new BufferProvider();
+            dso.Init(bp);
         }
 
         /// <summary>
@@ -98,6 +68,7 @@ namespace NesEmu {
         public void Reset() {
             APU.NESHardReset();
             CPU.Reset();
+            bp.Clear();
         }
 
         public void PlayFrameSamples() {
@@ -106,28 +77,67 @@ namespace NesEmu {
             APU.sampleclock = 0;
 
             var numAvailable = Bus.blip.SamplesAvailable();
-            var samples = new short[numAvailable * 2];
-            Bus.blip.ReadSamples(samples, numAvailable, true);
+            var samples = new short[SamplesPerFrame];
+            Bus.blip.ReadSamples(samples, numAvailable, false);
 
-            //Copies the signal to stereo
-            for (int i = 0; i < numAvailable * 2; i += 2)
-                samples[i +1] = samples[i];
-
-            var numQueued = SDL_GetQueuedAudioSize((uint)audioDevice);
-            // About 2 frames worth of samples
-            if (numQueued < 2940) {
-                unsafe {
-                    fixed (short* ptr = samples) {
-                        IntPtr intPtr = new(ptr);
-                        SDL_QueueAudio((uint)audioDevice, intPtr, (uint)(numAvailable * 4));
-                    }
-                }
+            if (numAvailable != SamplesPerFrame) {
+                samples = Resample(samples, numAvailable, SamplesPerFrame);
             }
 
+            if (bp.internalBuffer.Count == 0) {
+                Console.WriteLine("Audio has drifted and buffer is empty");
+            }
+            if (bp.internalBuffer.Count <= SamplesPerFrame * 10) {
+                bp.TryQueue(samples);
+            }
+            if (dso.PlaybackState == PlaybackState.Stopped) {
+                dso.Play();
+            }
         }
 
         public void Dispose() {
-            SDL_CloseAudioDevice((uint)audioDevice);
+            bp.Clear();
+            dso.Dispose();
+        }
+
+        // Taken from BizHawk, their license should be in my license file - Tromo
+        // This uses simple linear interpolation which is supposedly not a great idea for
+        // resampling audio, but it sounds surprisingly good to me. Maybe it works well
+        // because we are typically stretching by very small amounts.
+        private short[] Resample(short[] input, int inputCount, int outputCount) {
+            if (inputCount == outputCount) {
+                return input;
+            }
+
+            int channels = 1;
+
+            short[] output = new short[outputCount * channels];
+
+            if (inputCount == 0 || outputCount == 0) {
+                Array.Clear(output, 0, outputCount * channels);
+                return output;
+            }
+
+            for (int iOutput = 0; iOutput < outputCount; iOutput++) {
+                double iInput = ((double)iOutput / (outputCount - 1)) * (inputCount - 1);
+                int iInput0 = (int)iInput;
+                int iInput1 = iInput0 + 1;
+                double input0Weight = iInput1 - iInput;
+                double input1Weight = iInput - iInput0;
+
+                if (iInput1 == inputCount)
+                    iInput1 = inputCount - 1;
+
+                for (int iChannel = 0; iChannel < channels; iChannel++) {
+                    double value =
+                        input[iInput0 * channels + iChannel] * input0Weight +
+                        input[iInput1 * channels + iChannel] * input1Weight;
+
+                    output[iOutput * channels + iChannel] = (short)((int)(value + 32768.5) - 32768);
+                }
+            }
+
+            return output;
         }
     }
 }
